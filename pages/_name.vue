76cb58a18ed06635bf38bb29046f5f83c46d00d9
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2022-12-29 14:57:25 -0400

    fix test

--- pages/_name.vue
+++ pages/_name.vue
@@ -18,38 +18,21 @@ export default {
     return {
       title: "test de " + this.name,
       meta: [
+        { hid: "og-title", property: "og:title", content: "My Title" },
         {
-          hid: "og:title",
-          name: "og:title",
-          content: "test",
+          hid: "og-url",
+          property: "og:url",
+          content:
+            "http://ec2-44-212-0-107.compute-1.amazonaws.com:3001/" + this.name,
         },
+        { hid: "og-type", property: "og:type", content: "website" },
         {
-          hid: "og:description",
+          hid: "og-desc",
           property: "og:description",
           content: "test de " + this.name,
         },
         {
-          hid: "og:type",
-          property: "og:type",
-          content: "website",
-        },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: "http://ec2-44-212-0-107.compute-1.amazonaws.com:3001/",
-        },
-        {
-          hid: "og:title",
-          property: "og:title",
-          content: "test",
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: "Test de Foto",
-        },
-        {
-          hid: "og:image",
+          hid: "og-image",
           property: "og:image",
           content:
             "https://chgsdev.shareity.com/campaign/" + this.name + ".png",
