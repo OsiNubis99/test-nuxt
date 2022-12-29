e7c64b5a4c04ca2d794a915d2c01ec51ab64a9a2
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2022-12-29 14:45:15 -0400

    twitter meta

--- pages/_name.vue
+++ pages/_name.vue
@@ -55,29 +55,32 @@ export default {
             "https://chgsdev.shareity.com/campaign/" + this.name + ".png",
         },
         {
+          hid: "twitter:type",
+          property: "og:type",
+          content: "website",
+        },
+        {
+          hid: "twitter:url",
+          property: "og:url",
+          content: "http://ec2-44-212-0-107.compute-1.amazonaws.com:3001/",
+        },
+        {
+          hid: "twitter:title",
+          property: "og:title",
+          content: "test",
+        },
+        {
+          hid: "og:description",
+          property: "twitter:description",
+          content: "Test de Foto",
+        },
+        {
           hid: "twitter:image",
           property: "twitter:image",
           content:
             "https://chgsdev.shareity.com/campaign/" + this.name + ".png",
         },
       ],
-      // 			`<!-- Primary Meta Tags -->
-      // <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-
-      // <!-- Open Graph / Facebook -->
-      // <meta property="og:type" content="website">
-      // <meta property="og:url" content="https://metatags.io/">
-      // <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
-      // <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-      // <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-
-      // <!-- Twitter -->
-      // <meta property="twitter:card" content="summary_large_image">
-      // <meta property="twitter:url" content="https://metatags.io/">
-      // <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
-      // <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-      // <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-      // `
     };
   },
 };
